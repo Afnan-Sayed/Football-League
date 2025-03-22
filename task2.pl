8879ece0fc292d44b9958b89522452d7823b57e1
Afnan Sayed <afnansayed50@gmail.com>
2025-03-22 11:36:14 +0200

    Update task2

--- task2.pl
+++ task2.pl
@@ -1,28 +1,39 @@
+%%%%%%%%%%%% NOT_MY_MEMBER %%%%%%%%%%%%
+% Base Case
+not_my_member(_, []):-
+    !.
+
+% Recursive Case
+not_my_member(X, [H|T]):- 
+    X \= H, 
+    not_my_member(X, T).
+
+
+%%%%%%%%%%%% LIST_LENGTH %%%%%%%%%%%%
+list_length(L,N):-
+    list_length_acc(L, 0 ,N).
+
+%% BaseCase
+list_length_acc([], N ,N).
+
+%% RecursiveCase
+list_length_acc([_|T], N,Res):-
+    NewN is N+1,
+    list_length_acc(T,NewN,Res).
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% TASK2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% count how many teams are from a specific country
+team_count_by_country(Country,N):-          %the interface the user queries through
+    teams_count_by_country_acc(Country, Teams, []),  %the choosen country
+    list_length(Teams,N).            % NO. of teams with same con
 
-
-% Main predicate to counts teams from specific country
-team_count_by_country(Country, Count) :-
-    get_teams_by_country(Country, Teams, []),  %the choosen country
-    list_count(Teams, Count).                      % NO of teams with same con
-
-
-get_teams_by_country(Country,  TeamsList,  Accu) :-
-    team(Name, Country,  _),              % search database
-    \+ member(Name, Accu),                % no du
+%% RecursiveCase
+teams_count_by_country_acc(Country,Teams, Accu):-
+    team(Name,Country,  _),          
+    not_my_member(Name, Accu),                % no du
     !,
-    get_teams_by_country(Country, TeamsList, [Name|Accu]).  % rec call
+    teams_count_by_country_acc(Country,Teams, [Name|Accu]).
 
-% Base case: return the acc if no more match
-get_teams_by_country(_, TeamsList, TeamsList).
-
-%to count number teams in list
-list_count([], 0).
-list_count([_|T], Count) :-
-    list_count(T, TCount),
-    Count is  TCount  +  1.
-
-% Member to prevent dup
-member(X, [X|_]).
-member(X, [_|T]) :-
-    member(X, T).
+%% BaseCase when no more matches
+teams_count_by_country_acc(_, Teams, Teams).
